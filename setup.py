d3c10cb0a61ff5e4edb3469f44320a972761fd8c
sbkit <sven.bulach96gmail.com>
2024-11-04 17:16:11 +0100

    Project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+name='end_to_end_mlpipeline',
+version='0.0.1',
+author='Sven',
+packages=find_packages(),
+install_requires=['pandas', 'numpy', 'seaborn'],
+
+)
