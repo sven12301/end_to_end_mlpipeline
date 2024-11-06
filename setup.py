7db49c1135a189bd597c75527e7d9d5869cf58bf
sbkit <sven.bulach96gmail.com>
2024-11-06 19:40:20 +0100

    "Setup done"

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    Function returns the list of requirements 
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "")for req in requirements]
+
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
 name='end_to_end_mlpipeline',
 version='0.0.1',
 author='Sven',
 packages=find_packages(),
-install_requires=['pandas', 'numpy', 'seaborn'],
+install_requires=get_requirements('requirements.txt'),
 
 )
